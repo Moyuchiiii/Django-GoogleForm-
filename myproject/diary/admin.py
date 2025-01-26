816d3fc3e02ea4b1a222332b91b44a18590243d1
Moyuchiiii <yuta.oct.777@gmail.com>
2025-01-26 23:50:16 +0900

    作成者を記録するように修正

--- myproject/diary/admin.py
+++ myproject/diary/admin.py
@@ -4,4 +4,4 @@ from .models import Page
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
-    readonly_fields = ["answertf", "id", "created_at", "updated_at"]
+    readonly_fields = ["created_by", "answertf", "id", "created_at", "updated_at"]
--- myproject/diary/models.py
+++ myproject/diary/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 
 class Page(models.Model):
     answertf = models.BooleanField(default=False, verbose_name="回答")
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ID")
-    title =  models.CharField(max_length=100,verbose_name="タイトル")
+    title = models.CharField(max_length=100, verbose_name="タイトル")
     q1 = models.CharField(max_length=100, verbose_name="質問1", default="")
     q2 = models.CharField(max_length=100, verbose_name="質問2", default="", blank=True)
     q3 = models.CharField(max_length=100, verbose_name="質問3", default="", blank=True)
@@ -26,9 +27,10 @@ class Page(models.Model):
     a8 = models.TextField(max_length=2000, verbose_name="回答8", default="")
     a9 = models.TextField(max_length=2000, verbose_name="回答9", default="")
     a10 = models.TextField(max_length=2000, verbose_name="回答10", default="")
-    page_date = models.DateTimeField(auto_now_add=True,verbose_name="日付")
-    created_at = models.DateTimeField(auto_now_add=True,verbose_name="作成日時")
-    updated_at = models.DateTimeField(auto_now=True,verbose_name="更新日時")
+    page_date = models.DateTimeField(auto_now_add=True, verbose_name="日付")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="作成者", default=1)
 
     def __str__(self):
         return self.title
